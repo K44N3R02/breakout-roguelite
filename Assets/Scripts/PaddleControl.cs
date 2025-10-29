5e6a85a8d706ed22018ffa650809e7d638f59884
k44n <kaanemireroz@gmail.com>
2025-10-29 13:27:27 +0300

    initial setup of game scene

---
+++ Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Paddle Control Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
