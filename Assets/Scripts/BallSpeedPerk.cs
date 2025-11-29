1584de6db266b9028d509f83619224b93008d9e7
k44n <kaanemireroz@gmail.com>
2025-11-29 09:01:24 +0300

    add randomization of positive and negative perks

--- Assets/Scripts/BallSpeedPerk.cs
+++ Assets/Scripts/BallSpeedPerk.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class BallSpeedPerk : MonoBehaviour
 {
+    [System.Serializable]
+    struct ModifierAndChance
+    {
+        public float modifier;
+        public int relativeChance;
+    }
+
     [SerializeField] private Sprite speedUpSprite;
     [SerializeField] private Sprite speedDownSprite;
-    [SerializeField] private float speedChange = 10f;
+    [SerializeField] private ModifierAndChance modifier1;
+    [SerializeField] private ModifierAndChance modifier2;
     [SerializeField] private BallConfig ballConfig;
 
     private SpriteRenderer spriteRenderer;
+    private float speedChange;
 
     private void Start()
     {
+        float random = Random.Range(0f, 1f);
+        float mod1chance = ((float)modifier1.relativeChance) / (modifier1.relativeChance + modifier2.relativeChance);
+        speedChange = (random < mod1chance) ? modifier1.modifier : modifier2.modifier;
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = (speedChange >= 0) ? speedUpSprite : speedDownSprite;
     }
--- Assets/Scripts/PaddleWidthPerk.cs
+++ Assets/Scripts/PaddleWidthPerk.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class PaddleWidthPerk : MonoBehaviour
 {
+    [System.Serializable]
+    struct ModifierAndChance
+    {
+        public float modifier;
+        public int relativeChance;
+    }
+
     [SerializeField] private Sprite expandSprite;
     [SerializeField] private Sprite shrinkSprite;
-    [SerializeField] private float widthModifier = 0.5f;
+    [SerializeField] private ModifierAndChance modifier1;
+    [SerializeField] private ModifierAndChance modifier2;
 
     private SpriteRenderer spriteRenderer;
+    private float widthModifier;
 
     private void Start()
     {
+        float random = Random.Range(0f, 1f);
+        float mod1chance = ((float)modifier1.relativeChance) / (modifier1.relativeChance + modifier2.relativeChance);
+        widthModifier = (random < mod1chance) ? modifier1.modifier : modifier2.modifier;
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = (widthModifier >= 0) ? expandSprite : shrinkSprite;
     }
